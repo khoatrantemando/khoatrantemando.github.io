c45ce5f868e3d91eaa813ff545bd73f9b0697c32
Khoa Tran <khoa.tran@temando.com>
2015-02-10 13:14:47 +1000

    first commit

---
+++ assets/javascripts/app/layout.js
@@ -0,0 +1,1 @@
+!function(){$(window).scroll(function(){$(this).scrollTop()>45?($(".tocify-wrapper").addClass("fixed"),console.log("fixed!")):($(".tocify-wrapper").removeClass("fixed"),console.log("scroll"))})}(window);
